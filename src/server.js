3f674ab7334c9512c118697a9d08990b4d7f554e
TerexFed <terexfed9@gmail.com>
2025-04-18 19:02:33 +0400

    token get request and encryption

--- src/server.js
+++ src/server.js
@@ -5,24 +5,25 @@ import session from "express-session";
 import { runCrawler } from "../dist/main.js";
 import NodeCache from "node-cache";
 import { EventEmitter } from "events";
+import jsonWebToken from "jsonwebtoken";
+import fs from "fs";
 
 const PORT = 3000;
 const app = express();
+const jwt = jsonWebToken;
 app.use(cors());
 
 const cache = new NodeCache({ stdTTL: 3600 });
 
-
 app.use(
   session({
-    secret: "123", 
+    secret: "123",
     resave: false,
     saveUninitialized: true,
   })
 );
 
 app.use(bodyParser.json());
-
 
 app.get("/search", async (req, res) => {
   const { userInput } = req.query;
@@ -43,7 +44,6 @@ app.get("/search", async (req, res) => {
 
   const cachedResults = cache.get(TuserInput);
   if (cachedResults) {
- 
     res.write(`data: {"message": "Returning cached results"}\n\n`);
     cachedResults.forEach((result) => {
       const eventData = JSON.stringify(result);
@@ -51,10 +51,9 @@ app.get("/search", async (req, res) => {
       res.write(`data: ${eventData}\n\n`);
     });
 
-    res.write(`data: {"done": true}\n\n`); 
-    return res.end(); 
+    res.write(`data: {"done": true}\n\n`);
+    return res.end();
   }
-
 
   res.write(
     `data: {"message": "Search started, results will be sent in real time."}\n\n`
@@ -63,26 +62,40 @@ app.get("/search", async (req, res) => {
   try {
     const eventEmitter = new EventEmitter();
 
-
     eventEmitter.on("newResult", (result) => {
       const eventData = JSON.stringify(result);
       req.session.results.push(result);
-      
+
       const currentCached = cache.get(TuserInput) || [];
       cache.set(TuserInput, [...currentCached, result]);
       res.write(`data: ${eventData}\n\n`);
     });
 
     eventEmitter.on("done", () => {
-      res.write(`data: {"done": true}\n\n`); 
-      res.end(); 
+      res.write(`data: {"done": true}\n\n`);
+      res.end();
     });
 
-    
     await runCrawler(TuserInput, req.session, eventEmitter);
   } catch (error) {
     console.error("Error running crawler:", error);
     res.write(`data: {"error": "Failed to run crawler"}\n\n`);
+  }
+});
+
+app.get("/token", async (req, res) => {
+  try {
+    const privateKey = fs.readFileSync("./src/private.key");
+    const payload = {
+      token:
+        "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",
+      iat: Math.floor(Date.now() / 1000),
+    };
+    const signedToken = jwt.sign(payload, privateKey, { algorithm: "RS256" });
+    res.send(signedToken);
+  } catch (error) {
+    console.error("Error generating token:", error);
+    res.status(500).json({ error: "Failed to generate token" });
   }
 });
 
